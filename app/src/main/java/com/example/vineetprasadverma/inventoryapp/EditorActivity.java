37ee055a698fe7182e81ebd974fb09c01b85c09f
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-08-16 15:39:29 +0530

    Add save book method to insert or update pet from the EditorActivity

--- app/src/main/java/com/example/vineetprasadverma/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/vineetprasadverma/inventoryapp/EditorActivity.java
@@ -1,12 +1,20 @@
 package com.example.vineetprasadverma.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.vineetprasadverma.inventoryapp.data.ProductContract.ProductEntry;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -18,6 +26,18 @@ public class EditorActivity extends AppCompatActivity {
 
     private Uri mCurrentProductUri;
 
+    private boolean mProductHasChanged = false;
+
+    // OnTouchListener that listens for any user touches on a View, implying that they are modifying
+    // the view, and we change the mProductHasChanged boolean to true.
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mProductHasChanged = true;
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +46,84 @@ public class EditorActivity extends AppCompatActivity {
         Intent intent = getIntent();
         mCurrentProductUri = intent.getData();
 
-        if(mCurrentProductUri == null){
+        if (mCurrentProductUri == null) {
             setTitle(getString(R.string.add_book));
 
             // Invalidate the options menu, so the "Delete" menu option can be hidden.
             // (It doesn't make sense to delete a pet that hasn't been created yet.)
             invalidateOptionsMenu();
-        }else{
+        } else {
             setTitle(getString(R.string.edit_book));
         }
 
+        mBookEditText = findViewById(R.id.book_name_edit_text);
+        mPriceEditText = findViewById(R.id.price_edit_text);
+        mQantityEditText = findViewById(R.id.quantity_edit_text);
+        mSupplierNameEditText = findViewById(R.id.supplier_name_edit_text);
+        mSupplierPhoneNoEditText = findViewById(R.id.phone_no_edit_text);
+
+        mBookEditText.setOnTouchListener(mTouchListener);
+        mPriceEditText.setOnTouchListener(mTouchListener);
+        mQantityEditText.setOnTouchListener(mTouchListener);
+        mSupplierNameEditText.setOnTouchListener(mTouchListener);
+        mSupplierPhoneNoEditText.setOnTouchListener(mTouchListener);
+
+    }
+
+    //Get user inout from editor and save new book into database.
+    public void saveBook() {
+        // Read from input fields.
+        String bookString = mBookEditText.getText().toString().trim();
+        String priceString = mPriceEditText.getText().toString().trim();
+        String quantityString = mQantityEditText.getText().toString().trim();
+        String supplierNameString = mSupplierNameEditText.getText().toString().trim();
+        String supplierPhoneNoString = mSupplierPhoneNoEditText.getText().toString().trim();
+
+
+        // Check if this is supposed to be a new book
+        // and check if all the fields in the editor are blank.
+        if (mCurrentProductUri == null &&
+                TextUtils.isEmpty(bookString) && TextUtils.isEmpty(priceString) &&
+                TextUtils.isEmpty(quantityString) && TextUtils.isEmpty(supplierNameString) &&
+                TextUtils.isEmpty(supplierPhoneNoString)) {
+            Toast.makeText(this, getString(R.string.cant_save_empty_book), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create a ContentValues object where column names are the keys,
+        // and pet attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, bookString);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, priceString);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, quantityString);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME, supplierNameString);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER, supplierPhoneNoString);
+
+        if (mCurrentProductUri == null) {
+            Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, values);
+            if (newUri == null) {
+                Toast.makeText(EditorActivity.this, getString(R.string.editor_insert_product_failed), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(EditorActivity.this, getString(R.string.editor_insert_product_successful), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Otherwise this is an EXISTING book, so update the book with content URI: mCurrentProductUri
+            // and pass in the new ContentValues. Pass in null for the selection and selection args
+            // because mCurrentProductUri will already identify the correct row in the database that
+            // we want to modify.
+            int rowsAffected = getContentResolver().update(mCurrentProductUri, values, null, null);
+
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsAffected == 0) {
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, getString(R.string.editor_update_product_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_update_product_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -62,10 +150,22 @@ public class EditorActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_save:
+                saveBook();
+                finish();
                 return true;
 
             case R.id.action_delete:
                 return true;
+
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // If the pet hasn't changed, continue with navigating up to parent activity
+                // which is the {@link CatalogActivity}.
+                if (!mProductHasChanged) {
+                    NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                    return true;
+                }
+
         }
         return super.onOptionsItemSelected(item);
     }
