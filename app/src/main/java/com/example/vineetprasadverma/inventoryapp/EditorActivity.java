81878dfb801281c2e3259cc15b433cb9822ad3b3
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-08-16 14:50:01 +0530

    Programatically set correct name to the Editor activity when have to add book display ADD BOOK or when edit the activity display the EDIT BOOK

--- app/src/main/java/com/example/vineetprasadverma/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/vineetprasadverma/inventoryapp/EditorActivity.java
@@ -1,16 +1,36 @@
 package com.example.vineetprasadverma.inventoryapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private EditText mBookEditText;
+    private EditText mPriceEditText;
+    private EditText mQantityEditText;
+    private EditText mSupplierNameEditText;
+    private EditText mSupplierPhoneNoEditText;
+
+    private Uri mCurrentProductUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editor_activity);
+
+        Intent intent = getIntent();
+        mCurrentProductUri = intent.getData();
+
+        if(mCurrentProductUri == null){
+            setTitle(getString(R.string.add_book));
+        }else{
+            setTitle(getString(R.string.edit_book));
+        }
 
     }
 
